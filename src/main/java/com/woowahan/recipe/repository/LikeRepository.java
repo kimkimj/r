86cc5f5a4bdb50dbaace92e0d6c529a0bdefa13b
이소영 <th42500@naver.com>
2023-01-18 00:05:34 +0900

    [Fix] LikeRepository - id 자료형 수정

--- src/main/java/com/woowahan/recipe/repository/LikeRepository.java
+++ src/main/java/com/woowahan/recipe/repository/LikeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LikeRepository extends JpaRepository<LikeEntity, Integer> {  // FIXME: 2023-01-17  ID 자료형 수정
+public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
 }
