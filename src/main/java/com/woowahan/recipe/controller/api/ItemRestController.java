2e728fb2c1f828c0b8e6d614661c3468af8c5528
sujin <slrkajsep337@naver.com>
2023-01-19 01:33:09 +0900

    [Feat] 관리자 권한 확인 로직 추가

--- src/main/java/com/woowahan/recipe/controller/api/ItemRestController.java
+++ src/main/java/com/woowahan/recipe/controller/api/ItemRestController.java
@@ -1,31 +1,31 @@
 package com.woowahan.recipe.controller.api;
 
 import com.woowahan.recipe.domain.dto.Response;
-import com.woowahan.recipe.domain.dto.item.ItemCreateReqDto;
-import com.woowahan.recipe.domain.dto.item.ItemCreateResDto;
+import com.woowahan.recipe.domain.dto.itemDto.ItemCreateReqDto;
+import com.woowahan.recipe.domain.dto.itemDto.ItemCreateResDto;
 import com.woowahan.recipe.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/items")
+@RequestMapping("/api/v1/items")
 public class ItemRestController {
 
     private final ItemService itemService;
 
     /**
-     * 재료 추가(관리자)
+     * 재료 등록(관리자)
      */
-    //관리자 권한 확인 추가 해줘야함
     @PostMapping
-    public Response<ItemCreateResDto> createItem(@RequestBody ItemCreateReqDto itemCreateReqDto) {
-        return Response.success(itemService.createItem(itemCreateReqDto));
+    public Response<ItemCreateResDto> createItem(@RequestBody ItemCreateReqDto itemCreateReqDto, Authentication authentication) {
+        return Response.success(itemService.createItem(itemCreateReqDto, authentication.getName()));
     }
+
+
+
 
 
 
--- src/main/java/com/woowahan/recipe/service/ItemService.java
+++ src/main/java/com/woowahan/recipe/service/ItemService.java
@@ -1,23 +1,46 @@
 package com.woowahan.recipe.service;
 
-import com.woowahan.recipe.domain.dto.item.ItemCreateReqDto;
-import com.woowahan.recipe.domain.dto.item.ItemCreateResDto;
+import com.woowahan.recipe.domain.dto.itemDto.ItemCreateReqDto;
+import com.woowahan.recipe.domain.dto.itemDto.ItemCreateResDto;
 import com.woowahan.recipe.domain.entity.ItemEntity;
+import com.woowahan.recipe.domain.entity.UserEntity;
+import com.woowahan.recipe.exception.AppException;
+import com.woowahan.recipe.exception.ErrorCode;
 import com.woowahan.recipe.repository.ItemRepository;
+import com.woowahan.recipe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import static com.woowahan.recipe.domain.UserRole.ADMIN;
+import static com.woowahan.recipe.domain.UserRole.HEAD;
 
 
 @RequiredArgsConstructor
 @Service
 public class ItemService {
 
+    private final UserRepository userRepository;
     private final ItemRepository itemRepository;
 
+    //[중복 로직] user 존재 확인 + 가져오기
+    public UserEntity validateUser(String userName) {
+        return userRepository.findByUserName(userName)
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
+    }
+    //[중복 로직] 관리자 권한 확인
+    public void validateAdmin(UserEntity user) {
+        if(user.getUserRole() != HEAD && user.getUserRole() != ADMIN) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION, ErrorCode.INVALID_PERMISSION.getMessage());
+        }
+    }
+
     /**
-     * 재료 추가(관리자)
+     * 재료 등록(관리자)
      */
-    public ItemCreateResDto createItem(ItemCreateReqDto itemCreateReqDto) {
+    public ItemCreateResDto createItem(ItemCreateReqDto itemCreateReqDto, String userName) {
+        UserEntity user = validateUser(userName); //user 존재 확인
+        validateAdmin(user); //관리자 권한 확인
         ItemEntity item = itemCreateReqDto.toEntity();
         ItemEntity savedItem = itemRepository.save(item);
         return ItemCreateResDto.from(savedItem);
