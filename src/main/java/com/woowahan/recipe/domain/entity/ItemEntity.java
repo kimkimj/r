ba22d9422fbc173643494b013ab7063fed1459f0
sujin <slrkajsep337@naver.com>
2023-01-18 15:09:21 +0900

    [Feat] item entity, repository

---
+++ src/main/java/com/woowahan/recipe/domain/entity/ItemEntity.java
@@ -0,0 +1,35 @@
+package com.woowahan.recipe.domain.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+public class ItemEntity {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String itemImagePath;
+    private String itemName;
+    private String itemPrice;
+    private String itemStock;
+
+
+    //필요한지 불확실
+//    @OneToMany
+//    @JoinColumn(name = "recipe_id")
+//    private List<RecipeEntity> recipeList;
+
+
+}
