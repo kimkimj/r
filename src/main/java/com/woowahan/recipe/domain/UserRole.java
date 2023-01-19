e0a023184cbb4c1361540cbf0f1d6eef4994f1d7
sujin <slrkajsep337@naver.com>
2023-01-20 00:58:54 +0900

    [Feat] UserRole에 판매자 추가

--- src/main/java/com/woowahan/recipe/domain/UserRole.java
+++ src/main/java/com/woowahan/recipe/domain/UserRole.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 public enum UserRole {
     HEAD("role_head"),
     ADMIN("role_admin"),
+    SELLER("role_seller"),
     USER("role_user");
     private String value;
 }
