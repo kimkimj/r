038654764ccd1de1513e0bab62ff6bd99941c42e
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-01-19 16:54:32 +0900

    [Feat] 1. 배송정보 추가 로직 수정 2. 주문에 유저 추가 로직 수정 3. 에러코드 NOT_ENOUGH_STOCK 추가

--- src/main/java/com/woowahan/recipe/exception/ErrorCode.java
+++ src/main/java/com/woowahan/recipe/exception/ErrorCode.java
@@ -17,8 +17,9 @@ public enum ErrorCode {
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 에러입니다."),
     RECIPE_NOT_FOUND(HttpStatus.NOT_FOUND, "레시피를 찾을 수 없습니다."),
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "리뷰를 찾을 수 없습니다."),
-    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "상품이 존재하지 않습니다"),
-    EMPTY_CONTENT(HttpStatus.BAD_REQUEST, "내용이 없습니다.");
+    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "재료가 존재하지 않습니다"),
+    EMPTY_CONTENT(HttpStatus.BAD_REQUEST, "내용이 없습니다."),
+    NOT_ENOUGH_STOCK(HttpStatus.BAD_REQUEST, "재고 수량이 없습니다.");
 
     private HttpStatus httpStatus;
     private String message;
