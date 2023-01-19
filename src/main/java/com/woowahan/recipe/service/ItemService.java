c21655c93410743a86af8237ebab542d73aa1c08
sujin <slrkajsep337@naver.com>
2023-01-19 10:57:59 +0900

    [Feat] 아이템(재료) 수정 기능(u)

--- src/main/java/com/woowahan/recipe/service/ItemService.java
+++ src/main/java/com/woowahan/recipe/service/ItemService.java
@@ -2,6 +2,8 @@ package com.woowahan.recipe.service;
 
 import com.woowahan.recipe.domain.dto.itemDto.ItemCreateReqDto;
 import com.woowahan.recipe.domain.dto.itemDto.ItemCreateResDto;
+import com.woowahan.recipe.domain.dto.itemDto.ItemUpdateReqDto;
+import com.woowahan.recipe.domain.dto.itemDto.ItemUpdateResDto;
 import com.woowahan.recipe.domain.entity.ItemEntity;
 import com.woowahan.recipe.domain.entity.UserEntity;
 import com.woowahan.recipe.exception.AppException;
@@ -34,16 +36,35 @@ public class ItemService {
             throw new AppException(ErrorCode.INVALID_PERMISSION, ErrorCode.INVALID_PERMISSION.getMessage());
         }
     }
+    // item 존재 확인 + 가져오기
+    public ItemEntity validateItem(Long itemId) {
+        return itemRepository.findById(itemId)
+                .orElseThrow(() -> new AppException(ErrorCode.Item_NOT_FOUND, ErrorCode.Item_NOT_FOUND.getMessage()));
+    }
 
     /**
      * 재료 등록(관리자)
      */
-    public ItemCreateResDto createItem(ItemCreateReqDto itemCreateReqDto, String userName) {
+    public ItemCreateResDto createItem(ItemCreateReqDto ReqDto, String userName) {
         UserEntity user = validateUser(userName); //user 존재 확인
         validateAdmin(user); //관리자 권한 확인
-        ItemEntity item = itemCreateReqDto.toEntity();
+        ItemEntity item = ReqDto.toEntity();
         ItemEntity savedItem = itemRepository.save(item);
         return ItemCreateResDto.from(savedItem);
     }
 
+    /**
+     * 재료 수정(관리자)
+     */
+    public ItemUpdateResDto updateItem(Long id, ItemUpdateReqDto ReqDto, String userName) {
+        UserEntity user = validateUser(userName); //user 존재 확인+가져오기
+        validateAdmin(user); //관리자 권한 확인
+        ItemEntity item = validateItem(id); //item 존재 확인+가져오기
+
+        item.update(ReqDto.getItemImagePath(), ReqDto.getItemName(), ReqDto.getItemPrice(), ReqDto.getItemStock());
+        ItemEntity savedItem = itemRepository.save(item);
+        itemRepository.flush();
+        return ItemUpdateResDto.from(savedItem);
+
+    }
 }
