9f4fc4e516e1dad84151131f73c3e1f01fcd3f3b
이소영 <th42500@naver.com>
2023-01-22 01:12:19 +0900

    [Refactor] SellerRepository - findByUser() 추가

--- src/main/java/com/woowahan/recipe/repository/SellerRepository.java
+++ src/main/java/com/woowahan/recipe/repository/SellerRepository.java
@@ -1,8 +1,14 @@
 package com.woowahan.recipe.repository;
 
+import com.woowahan.recipe.domain.entity.SellerEntity;
+import com.woowahan.recipe.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SellerRepository extends JpaRepository {
+
+   Optional<SellerEntity> findByUser(UserEntity user);
 }
