b363566e4f7c8824767552f81cdda8e9fd186b50
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-01-22 21:23:59 +0900

    [Feat] 에러코드 ORDER_NOT_FOUND 추가 및 RuntimeException AppExcepion 에러로 바꿈

--- src/main/java/com/woowahan/recipe/exception/AppException.java
+++ src/main/java/com/woowahan/recipe/exception/AppException.java
@@ -6,8 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class AppException extends RuntimeException{
+
     private ErrorCode errorCode;
     private String message;
 
+    @Override
+    public String toString() {
+        if (message == null) return errorCode.getMessage();
+        return message;
+    }
 }
 
