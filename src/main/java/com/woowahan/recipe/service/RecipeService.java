62e6153fbbaa73aa3be285782cc04d5e7cb0e7e1
이소영 <th42500@naver.com>
2023-01-25 03:22:57 +0900

    [Comment] RecipeService - 주석 수정

--- src/main/java/com/woowahan/recipe/service/RecipeService.java
+++ src/main/java/com/woowahan/recipe/service/RecipeService.java
@@ -148,6 +148,7 @@ public class RecipeService {
      * @date 2023-01-24
      * @return String
      * @description 좋아요를 처음 눌렀다면 "좋아요를 눌렀습니다.", 좋아요를 이미 누른 상태라면 "좋아요를 취소합니다." 반환
+     *              좋아요를 눌렀을 때 레시피 작성자와 좋아요를 누른 회원이 다르다면 NEW_LIKE_ON_RECIPE 알람 등록
     **/
     public String pushLikes(Long id, String userName) {
         UserEntity user = validateUserName(userName);
@@ -158,7 +159,7 @@ public class RecipeService {
             return "좋아요를 취소합니다.";
         }else {
             likeRepository.save(LikeEntity.of(user, recipe));
-            if(!user.equals(recipe.getUser())) {  // 현재 좋아요를 누른 사람과 레시피 작성자가 일치하지 않는 경우
+            if(!user.equals(recipe.getUser())) {  // 현재 좋아요를 누른 사람과 레시피 작성자가 일치하지 않다면 알람 등록
                 publisher.publishEvent(AlarmEvent.of(AlarmType.NEW_LIKE_ON_RECIPE, user, recipe.getUser()));
             }
             return "좋아요를 눌렀습니다.";
