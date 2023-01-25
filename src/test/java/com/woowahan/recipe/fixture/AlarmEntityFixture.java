7262c14badc250e9527506faf45e253613624d9e
이소영 <th42500@naver.com>
2023-01-25 15:09:36 +0900

    [Test] 알람 조회 성공, 실패 테스트 완료

--- src/test/java/com/woowahan/recipe/fixture/AlarmEntityFixture.java
+++ src/test/java/com/woowahan/recipe/fixture/AlarmEntityFixture.java
@@ -5,11 +5,9 @@ import com.woowahan.recipe.domain.entity.AlarmType;
 import com.woowahan.recipe.domain.entity.RecipeEntity;
 import com.woowahan.recipe.domain.entity.UserEntity;
 
-import java.time.LocalDateTime;
-
 public class AlarmEntityFixture {
 
-    public static AlarmEntity get(Long id, UserEntity fromUser, RecipeEntity recipe, AlarmType alarmType, LocalDateTime createdDate, LocalDateTime LastModifiedDate) {
+    public static AlarmEntity get(Long id, UserEntity fromUser, RecipeEntity recipe, AlarmType alarmType) {
         AlarmEntity alarm = AlarmEntity.builder()
                 .id(id)
                 .fromUser(fromUser)
