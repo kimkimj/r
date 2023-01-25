3e543c915e87d844c7b23067a4a2279e98ec2ba7
sujin <slrkajsep337@naver.com>
2023-01-25 16:41:30 +0900

    [Feat] 아이템 수정 페이지 구현

--- src/main/java/com/woowahan/recipe/controller/ui/ItemController.java
+++ src/main/java/com/woowahan/recipe/controller/ui/ItemController.java
@@ -1,6 +1,7 @@
 package com.woowahan.recipe.controller.ui;
 
 import com.woowahan.recipe.domain.dto.itemDto.ItemCreateReqDto;
+import com.woowahan.recipe.domain.dto.itemDto.ItemUpdateReqDto;
 import com.woowahan.recipe.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,7 +24,7 @@ public class ItemController {
     private final ItemService itemService;
 
     /**
-     * 재료 등록(관리자)
+     * 재료 등록(관리자, 판매자)
      */
     @GetMapping("/create")
     public String createForm(Model model) {
@@ -38,6 +40,26 @@ public class ItemController {
         }
         model.addAttribute("itemCreateReqDto", new ItemCreateReqDto());
         itemService.createItem(reqDto, authentication.getName());
+        return "redirect:/";
+    }
+
+    /**
+     * 재료 수정(관리자, 판매자)
+     */
+    @GetMapping("{id}/update")
+    public String updateForm(Model model) {
+        model.addAttribute("itemUpdateReqDto", new ItemUpdateReqDto());
+        return "item/updateForm";
+    }
+
+    @PostMapping("{id}/update")
+    public String update(@PathVariable Long id, @Validated ItemUpdateReqDto reqDto, BindingResult bindingResult, Model model, Authentication authentication) {
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult = {}", bindingResult);
+            return "item/updateForm";
+        }
+        model.addAttribute("itemUpdateReqDto", new ItemUpdateReqDto());
+        itemService.updateItem(id, reqDto, authentication.getName());
         return "redirect:/";
     }
 
