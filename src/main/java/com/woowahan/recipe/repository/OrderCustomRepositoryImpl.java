b26d0da38a83a067281f9e54a93e8c2db7e67426
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-01-26 17:17:16 +0900

    [Feat] 1. Querydsl 설정 2. 배송 상태에 따른 주문 내역 조회 기능 3. 상품 이름에 따른 주문 내역 조회 기능

---
+++ src/main/java/com/woowahan/recipe/repository/OrderCustomRepositoryImpl.java
@@ -0,0 +1,53 @@
+package com.woowahan.recipe.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.woowahan.recipe.domain.OrderStatus;
+import com.woowahan.recipe.domain.dto.orderDto.search.OrderSearch;
+import com.woowahan.recipe.domain.entity.OrderEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.woowahan.recipe.domain.entity.QDeliveryEntity.deliveryEntity;
+import static com.woowahan.recipe.domain.entity.QItemEntity.itemEntity;
+import static com.woowahan.recipe.domain.entity.QOrderEntity.orderEntity;
+import static com.woowahan.recipe.domain.entity.QOrderItemEntity.orderItemEntity;
+import static com.woowahan.recipe.domain.entity.QUserEntity.userEntity;
+
+@RequiredArgsConstructor
+@Repository
+public class OrderCustomRepositoryImpl implements OrderCustomRepository{
+
+    private final JPAQueryFactory queryFactory;
+
+    // 상품 주문 내역 조회
+    // 주문 진행, 주문 취소 조건 필요
+    // 상품 이름에 대한 조건 필요
+    @Override
+    public List<OrderEntity> findAllByString(OrderSearch orderSearch) {
+        return searchOrder(orderSearch.getOrderStatus(), orderSearch.getItemName());
+    }
+
+    private List<OrderEntity> searchOrder(OrderStatus orderStatusCond, String itemNameCond) {
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        if (orderStatusCond != null) {
+            booleanBuilder.and(orderEntity.orderStatus.eq(orderStatusCond));
+        }
+
+        if (itemNameCond != null) {
+            booleanBuilder.and(itemEntity.itemName.contains(itemNameCond));
+        }
+        return queryFactory.select(orderEntity)
+                .from(orderEntity)
+                .join(orderEntity.user, userEntity).fetchJoin()
+                .join(orderEntity.delivery, deliveryEntity).fetchJoin()
+                .join(orderEntity.orderItems, orderItemEntity).fetchJoin()
+                .join(orderItemEntity.item, itemEntity).fetchJoin()
+                .where(booleanBuilder)
+                .distinct()
+                .fetch();
+    }
+}
