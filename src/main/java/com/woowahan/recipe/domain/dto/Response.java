2031ae959f050970461c23e4547224d68f51c0a3
이소영 <th42500@naver.com>
2023-01-26 18:32:48 +0900

    [Chore] Response - <> 추가

--- src/main/java/com/woowahan/recipe/domain/dto/Response.java
+++ src/main/java/com/woowahan/recipe/domain/dto/Response.java
@@ -10,10 +10,10 @@ public class Response<T> {
     private T result;
 
     public static <T> Response<T> error(T result) {
-        return new Response("ERROR", result);
+        return new Response<>("ERROR", result);
     }
 
     public static <T> Response<T> success(T result) {
-        return new Response("SUCCESS", result);
+        return new Response<>("SUCCESS", result);
     }
 }
