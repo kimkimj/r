a59519555d80de02df181c385bf0d6ea1c874957
sujin <slrkajsep337@naver.com>
2023-01-26 13:24:51 +0900

    [Fix] @NotBlank -> @NotNull

--- src/main/java/com/woowahan/recipe/domain/entity/ItemEntity.java
+++ src/main/java/com/woowahan/recipe/domain/entity/ItemEntity.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -26,9 +27,9 @@ public class ItemEntity extends BaseEntity{
     private String itemImagePath;
     @NotBlank
     private String itemName;
-    @NotBlank
+    @NotNull
     private Integer itemPrice;
-    @NotBlank
+    @NotNull
     private Integer itemStock;
 
     @ManyToOne
