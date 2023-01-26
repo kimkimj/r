ae30c6c2c715f03b834e961f9192e0b2b822d7b6
이소영 <th42500@naver.com>
2023-01-26 23:47:09 +0900

    [Feat] CartRepository 생성

---
+++ src/main/java/com/woowahan/recipe/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.woowahan.recipe.repository;
+
+import com.woowahan.recipe.domain.entity.CartEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<CartEntity, Long> {
+}
