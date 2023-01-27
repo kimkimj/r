db2df8fb84482a9e5ebc2ff5c903fe1eb5e8e699
이소영 <th42500@naver.com>
2023-01-27 10:11:06 +0900

    [Feat] CartRepository 추가

--- src/main/java/com/woowahan/recipe/repository/CartRepository.java
+++ src/main/java/com/woowahan/recipe/repository/CartRepository.java
@@ -1,9 +1,13 @@
 package com.woowahan.recipe.repository;
 
 import com.woowahan.recipe.domain.entity.CartEntity;
+import com.woowahan.recipe.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<CartEntity, Long> {
+    Optional<CartEntity> findByUser(UserEntity user);
 }
