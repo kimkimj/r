9c741ef7b0c70d0be2a03d76fda45aab77463b04
이소영 <th42500@naver.com>
2023-01-27 10:10:49 +0900

    [Feat] 장바구니가 없는 경우 에러 코드 추가

--- src/main/java/com/woowahan/recipe/exception/ErrorCode.java
+++ src/main/java/com/woowahan/recipe/exception/ErrorCode.java
@@ -20,7 +20,8 @@ public enum ErrorCode {
     ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "재료가 존재하지 않습니다"),
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문이 존재하지 않습니다."),
     EMPTY_CONTENT(HttpStatus.BAD_REQUEST, "내용이 없습니다."),
-    NOT_ENOUGH_STOCK(HttpStatus.BAD_REQUEST, "재고 수량이 없습니다.");
+    NOT_ENOUGH_STOCK(HttpStatus.BAD_REQUEST, "재고 수량이 없습니다."),
+    CART_NOT_FOUND(HttpStatus.NOT_FOUND, "장바구니에 담긴 상품이 없습니다.");
 
     private HttpStatus httpStatus;
     private String message;
