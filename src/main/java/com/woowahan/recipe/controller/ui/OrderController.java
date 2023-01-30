3c472f17afe84dd859b7e5f68d329499ffd3ef1a
u <mjkim2304@gmail.com>
2023-01-30 15:22:50 +0900

    [Feat] 주문 페이지 UI

--- src/main/java/com/woowahan/recipe/controller/ui/OrderController.java
+++ src/main/java/com/woowahan/recipe/controller/ui/OrderController.java
@@ -1,17 +1,21 @@
 package com.woowahan.recipe.controller.ui;
 
 import com.woowahan.recipe.domain.dto.orderDto.OrderCreateReqDto;
+import com.woowahan.recipe.domain.dto.orderDto.OrderInfoResponse;
 import com.woowahan.recipe.domain.dto.userDto.UserJoinReqDto;
+import com.woowahan.recipe.domain.dto.userDto.UserResponse;
 import com.woowahan.recipe.domain.entity.DeliveryEntity;
 import com.woowahan.recipe.domain.entity.OrderEntity;
 import com.woowahan.recipe.domain.entity.OrderItemEntity;
 import com.woowahan.recipe.domain.entity.UserEntity;
+import com.woowahan.recipe.service.OrderService;
 import com.woowahan.recipe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -20,13 +24,16 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class OrderController {
 
-
-    @GetMapping("/placeOrder")
-    public String orderForm(Model model) {
-        model.addAttribute("deliveryEntity", new DeliveryEntity());
-        model.addAttribute("userEntity", new UserEntity());
-        model.addAttribute("orderEntity", new OrderEntity());
-        model.addAttribute("orderItemEntity", new OrderItemEntity());
+    private final OrderService orderService;
+    private final UserService userService;
+    @GetMapping("{userId}/placeOrder/{orderId}")
+    public String orderForm(@PathVariable Long userId, @PathVariable Long orderId, Model model) {
+        String username = "GordonRamsey";
+        OrderInfoResponse order = orderService.findOrder(username, orderId);
+        model.addAttribute("orderInfoResponse", order);
+        UserResponse userResponse = userService.findUser(userId);
+        model.addAttribute("userResponse", userResponse);
         return "order/orderForm";
     }
+
 }
