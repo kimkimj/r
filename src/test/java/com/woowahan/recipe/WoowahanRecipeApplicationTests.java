4eefbb0dcf92672f7035add96e5bf5b6730b1ed9
이소영 <th42500@naver.com>
2023-01-30 17:56:23 +0900

    [Test] 장바구니 조회 테스트, ContextLoads() 주석

--- src/test/java/com/woowahan/recipe/WoowahanRecipeApplicationTests.java
+++ src/test/java/com/woowahan/recipe/WoowahanRecipeApplicationTests.java
@@ -1,13 +1,12 @@
 package com.woowahan.recipe;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WoowahanRecipeApplicationTests {
 
-    @Test
+    /*@Test
     void contextLoads() {
-    }
+    }*/
 
 }
