b19eefd7b440dd2f72bf46ab382c6acc349def9d
이소영 <th42500@naver.com>
2023-01-31 10:28:23 +0900

    [Feat] 장바구니 조회 UI 진행중

--- src/main/java/com/woowahan/recipe/controller/ui/CartController.java
+++ src/main/java/com/woowahan/recipe/controller/ui/CartController.java
@@ -2,17 +2,22 @@ package com.woowahan.recipe.controller.ui;
 
 import com.woowahan.recipe.domain.dto.cartDto.CartInfoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/carts")
 public class CartController {
 
-    @GetMapping("/carts")
-    public String cartForm(Model model) {
-        model.addAttribute("CartInfo", new CartInfoResponse());
+    @GetMapping
+    public String cartForm(Model model, @PageableDefault(size = 20, sort = "name", direction = Sort.Direction.DESC) Pageable pageable) {
+        model.addAttribute("cartInfoResponse", new CartInfoResponse());
         return "cart/cartForm";
     }
 }
