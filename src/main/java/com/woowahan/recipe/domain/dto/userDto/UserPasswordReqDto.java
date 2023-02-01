35d801f5c1227b28b7df826441e0a33afeba45cc
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-02-01 16:04:51 +0900

    [Feat] 1. 회원정보 수정하기 2. 비밀번호 수정하기

---
+++ src/main/java/com/woowahan/recipe/domain/dto/userDto/UserPasswordReqDto.java
@@ -0,0 +1,14 @@
+package com.woowahan.recipe.domain.dto.userDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPasswordReqDto {
+    String password;
+}
