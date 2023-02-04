8d5e46d75d81a6ee7039fd77af4ca93cf46e6a2f
이소영 <th42500@naver.com>
2023-02-05 03:04:44 +0900

    [Feat] 장바구니 수량 변경 프론트 진행중

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,3 +1,54 @@
+var cartList;
+
+// 장바구니 상품 객체 -> 객체 배열에 담기
+$(document).ready(function () {
+    $.ajax({
+        url: "/carts",
+        type: "GET",
+        success : function (data) {
+            console.log(data.json().result);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+    console.log(cartList);
+})
+
+
+// 상품 배열 생성
+
+
+// IndexedDB 지원여부 확인
+if(!window.indexedDB) {
+    window.alert("해당 브라우저는 IndexedDB를 지원하지 않습니다.");
+} else {
+    var db;
+    var request = window.indexedDB.open('myCartDB');
+
+    request.onerror = function(event) {
+        console.log(event);
+        alert('failed');
+    }
+    request.onsuccess = function(event) {
+        console.log(event);
+        db = request.result;
+    }
+}
+
+// 상품 수량 변경하기
+function count(type) {
+    var request = window.indexedDB.open('myCartDB');
+
+    request.onerror = function (event) {
+        alert('Database Error: ' + event.target.errorCode);
+    }
+    // request.onsuccess = function(event) {
+    //     var transaction =db.transaction(['cartItem'], 'readwrite');
+    //
+    // }
+}
+
 // 총 결제 예정 금액 구하기
 function getCheckPrice() {
     console.log('getCheckPrice');
