9e5bfb48d4f1eaa4311865efe1b9fb4bdb661d99
이소영 <th42500@naver.com>
2023-02-06 17:52:31 +0900

    [Refactor] 장바구니 수량 변경 수정중

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,5 +1,38 @@
 // 상품 수량 변경하기
-function count(type) {
+function count(type, idx)  {
+    console.log('count() 실행');
+    console.log(idx);
+    let number = parseInt(document.getElementsByClassName('item-cnt')[0].value);
+    // 상품 재고
+    let stock = parseInt(document.getElementsByClassName('individual_stock_input')[0].value);
+    console.log('현재 수량 = ' + number);
+    console.log('재고 = ' + stock);
+    // 더하기/빼기
+    if(type === 'plus') {
+        console.log("plus");
+        number = number + 1;
+        if(number > 10) {
+            alert("수량은 10개 이하이어야 합니다.");
+            document.getElementsByClassName('item-cnt')[0].value = 10;
+            return;
+        }  else if (number > stock) {
+            alert("구매할 수 있는 개수를 초과하였습니다.");
+            return;
+        }
+    }else if(type === 'minus')  {
+        console.log("minus");
+        number = number - 1;
+        if(number <=0) {
+            alert("수량은 1개 이상이어야 합니다.");
+            document.getElementsByClassName('item-cnt')[0].value = 1;
+            return;
+        }
+    }
+
+    console.log("number = " + number);
+
+    // 결과 출력
+    document.getElementsByClassName('item-cnt')[0].value = number;
 }
 
 // 전체 선택
