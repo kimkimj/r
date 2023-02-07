5451a7fb3983df22279fc5fcb6fc855b1f5395fa
dmdwns2 <wjsdncl321@gmail.com>
2023-02-07 11:05:38 +0900

    [Feat] 장바구니 재료추가 백엔드 기능구현

--- src/main/java/com/woowahan/recipe/service/CartService.java
+++ src/main/java/com/woowahan/recipe/service/CartService.java
@@ -1,5 +1,6 @@
 package com.woowahan.recipe.service;
 
+import com.woowahan.recipe.domain.dto.cartDto.CartItemListReqDto;
 import com.woowahan.recipe.domain.dto.cartDto.CartItemReq;
 import com.woowahan.recipe.domain.dto.cartDto.CartItemResponse;
 import com.woowahan.recipe.domain.dto.cartDto.CartOrderDto;
@@ -161,5 +162,35 @@ public class CartService {
     private CartItemEntity validateCartItem(CartEntity cart, Long itemId) {
         return cartItemRepository.findByCartAndItemId(cart, itemId)
                 .orElseThrow(() -> new AppException(ErrorCode.CART_ITEM_NOT_FOUND, ErrorCode.CART_ITEM_NOT_FOUND.getMessage()));
+    }
+
+    public void addCartItemList(CartItemListReqDto cartItemListReqDto, String userName) {
+        UserEntity user = validateUser(userName); //user 존재 검증
+        CartEntity cart = validateCart(user); //user의 cart가 있는지, 존재 검증 -> 없으면 카트 생성
+        for (int i = 0; i < cartItemListReqDto.getItems().size(); i++) { // 리스트에서 하나씩 꺼내면서 item 생성, cart에 담기
+            ItemEntity item = validateItemList(cartItemListReqDto.getItems().get(i)); //카트에 넣으려는 아이템이 존재하는지 확인
+
+            Optional<CartItemEntity> cartItem = cartItemRepository.findByCartAndItemId(cart, item.getId());
+
+            if (cartItem.isEmpty()) {
+                if (item.getItemStock() < cartItemListReqDto.getCartItemCnt()) { //아이템 stock 충분한지 확인
+                    throw new AppException(ErrorCode.NOT_ENOUGH_STOCK, ErrorCode.NOT_ENOUGH_STOCK.getMessage());
+                }
+                CartItemEntity cartItemEntity = CartItemEntity.createCartItem(cartItemListReqDto.getCartItemCnt(), item, cart); //상품이 없으면 카트에 아이템 create
+                cartItemRepository.save(cartItemEntity);
+            } else {
+                Integer cnt = cartItem.get().getCartItemCnt() + cartItemListReqDto.getCartItemCnt();
+
+                if (item.getItemStock() < cnt) { //아이템 stock 충분한지 확인
+                    throw new AppException(ErrorCode.NOT_ENOUGH_STOCK, ErrorCode.NOT_ENOUGH_STOCK.getMessage());
+                }
+                cartItem.get().updateCartItemCnt(cnt); //상품이 이미 카트에 있으면 아이템수만 db에 update
+            }
+        }
+        //1일때 -하면 아이템 삭제하기
+    }
+    private ItemEntity validateItemList(String item) {
+        return itemRepository.findByName(item)
+                .orElseThrow(() -> new AppException(ErrorCode.ITEM_NOT_FOUND, ErrorCode.ITEM_NOT_FOUND.getMessage()));
     }
 }
