ab837727c65a0f36c4db9c7572ad063382588351
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-02-10 13:47:10 +0900

    [Test] CartItemReq 생성자 isCheck 값 추가

--- src/main/java/com/woowahan/recipe/controller/ui/ItemController.java
+++ src/main/java/com/woowahan/recipe/controller/ui/ItemController.java
@@ -62,7 +62,7 @@ public class ItemController {
 
         model.addAttribute("id", id);
         model.addAttribute("resDto", resDto); // -> sellerName도 들어있음
-        model.addAttribute("cartItemReq", new CartItemReq(id, 1));
+        model.addAttribute("cartItemReq", new CartItemReq(id, 1, true));
         return "item/findForm";
     }
 
