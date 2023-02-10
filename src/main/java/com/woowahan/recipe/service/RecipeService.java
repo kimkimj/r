43abc004cb5254681d337835a92d422f6031c7b3
dmdwns2 <wjsdncl321@gmail.com>
2023-02-10 14:14:49 +0900

    [Feat] 레시피 수정 문제 해결

--- src/main/java/com/woowahan/recipe/service/RecipeService.java
+++ src/main/java/com/woowahan/recipe/service/RecipeService.java
@@ -124,6 +124,7 @@ public class RecipeService {
         // TODO: 2023-01-24 를 사용하는 SnakeCase보다는 CamelCase가 Java 프로그래밍에서 권장되는 표기법이라고 합니다 🙂
         recipe.setRecipeTitle(recipeUpdateReqDto.getRecipeTitle());
         recipe.setRecipeBody(recipeUpdateReqDto.getRecipeBody());
+        recipeItemRepository.deleteAll();
         if(recipeUpdateReqDto.getRecipeImagePath() != null) {
             recipe.setRecipeImagePath(recipeUpdateReqDto.getRecipeImagePath());
         }
