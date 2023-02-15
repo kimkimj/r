69abe8827ad6ee10b5ef36fee41483749b6c7b4c
이소영 <th42500@naver.com>
2023-02-15 10:42:32 +0900

    [Fix] user, seller 회원가입 폼 수정

--- src/main/java/com/woowahan/recipe/domain/dto/userDto/UserJoinReqDto.java
+++ src/main/java/com/woowahan/recipe/domain/dto/userDto/UserJoinReqDto.java
@@ -4,10 +4,6 @@ import com.woowahan.recipe.domain.UserRole;
 import com.woowahan.recipe.domain.entity.UserEntity;
 import lombok.*;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 @Getter
 @Setter
 @Builder
@@ -15,32 +11,32 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 public class UserJoinReqDto {
 
-    @NotBlank(message = "아이디를 입력해주세요.")
-    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,8}$", message = "아이디는 8자 이하 한글, 영문 대소문자, 숫자를 사용하세요.")
+//    @NotBlank(message = "아이디를 입력해주세요.")
+//    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,8}$", message = "아이디는 8자 이하 한글, 영문 대소문자, 숫자를 사용하세요.")
     private String userName;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{8,24}$", message = "비밀번호는 8~24자 한글, 영문 대소문자, 숫자를 사용하세요.")
+//    @NotBlank(message = "비밀번호를 입력해주세요.")
+//    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{8,24}$", message = "비밀번호는 8~24자 한글, 영문 대소문자, 숫자를 사용하세요.")
     private String password;
 
-    @NotBlank(message = "이름을 입력해주세요.")
-    @Size(min = 2, max = 10, message = "이름은 2자 이상 16자 이하로 입력해주세요.")
+//    @NotBlank(message = "이름을 입력해주세요.")
+//    @Size(min = 2, max = 10, message = "이름은 2자 이상 16자 이하로 입력해주세요.")
     private String name;
 
-    @NotBlank(message = "주소를 입력해주세요.")
+//    @NotBlank(message = "주소를 입력해주세요.")
     private String address;
 
-    @NotBlank(message = "이메일 주소를 입력해주세요.")
-    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+//    @NotBlank(message = "이메일 주소를 입력해주세요.")
+//    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
-    @Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})", message = "010XXXXYYYY 형식으로 입력해주세요.")
+//    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
+//    @Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})", message = "010XXXXYYYY 형식으로 입력해주세요.")
     private String phoneNum;
 
 //    private UserRole userRole;
 
-    @NotBlank(message = "생년월일을 입력해주세요.")
+//    @NotBlank(message = "생년월일을 입력해주세요.")
     private String birth;
 
     // UserJoinReqeust가 User로 변환되는 로직
--- src/main/resources/static/js/sellerJoin.js
+++ src/main/resources/static/js/sellerJoin.js
@@ -19,16 +19,16 @@ $(document).ready(function () {
     });
 
     $(".id-btn").on("click", function () {
-        let username = $('#id-input').val();
-        if (username.trim().length == 0) {
+        let sellername = $('#id-input').val();
+        if (sellername.trim().length == 0) {
             $("#id-result").text("아이디를 입력해주세요").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
             return;
         }
         $.ajax({
-            url: "/api/v1/check-username",
+            url: "/api/v1/seller/check-sellerName",
             method: "GET",
             data: {
-                userName: username
+                sellerName: sellername
             },
             success: function (message) {
                 alert(message);
@@ -53,17 +53,17 @@ $(document).ready(function () {
     });
 
     $("#name-input").keyup(function () {
-        console.log("name 검사중");
+        console.log("company 검사중");
         let ckname = $("#name-input").val();
-        if (ckname.length < 2 || ckname.length > 16) {
-            $("#name-result").text("이름은 2자이상 16자이하여야 합니다").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
+        if (ckname.length < 2 || ckname.length > 20) {
+            $("#name-result").text("회사명은 2자이상 16자이하여야 합니다").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
         }else {
             $("#name-result").empty();
         }
     });
 
     $("#email-input").keyup(function () {
-        let emailCheck =/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        let emailCheck = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
         console.log("email 검사중");
         let ckemail = $("#email-input").val();
         if (ckemail.trim().length < 0) {
@@ -79,18 +79,18 @@ $(document).ready(function () {
     });
 
     $(".email-btn").on("click", function () {
-        let emailCheck =/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        let emailCheck =new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
         let ckemail = $('#email-input').val();
         if (ckemail.trim().length === 0 || ckemail == 'undefined') {
             $("#email-result").text("이메일을 입력해주세요").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
             return;
-        }else if(!emailCheck.test(ckemail) || ckemail.search(/\s/g)){
+        }else if(!emailCheck.test(ckemail) || ckemail.match(/\s/g)){
             $("#email-result").text("이메일을 올바른 형식으로 적어주세요.").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
             return;
         }
 
         $.ajax({
-            url: "/api/v1/check-email",
+            url: "/api/v1/seller/check-email",
             method: "GET",
             contentType: 'application/json',
             data: {
@@ -109,25 +109,35 @@ $(document).ready(function () {
     $("#phone-input").keyup(function () {
         console.log("phone 검사중");
         let ckphone = $("#phone-input").val();
-        if (ckphone.length < 0) {
+        let phoneCheck =new RegExp('(01[016789])(\\d{3,4})(\\d{4})');
+        if (ckphone.length <= 0) {
             $("#phone-result").text("핸드폰 번호를 입력해주세요").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
+            return;
         }else if(ckphone.search(/\W|\s/g) > -1) {
             $("#phone-result").text("핸드폰 번호는 특수기호, 공백 없이 숫자만 입력해주세요").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
+            return;
+        }else if(!phoneCheck.test(ckphone)){
+            $("#phone-result").text("010xxxxxxxx형식으로 입력해주세요").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
+            return;
         }else {
             $("#phone-result").empty();
+            return;
         }
     });
 
-    $("#birth-input").keyup(function () {
-        let birthCheck =/^(19[0-9][0-9]|20\d{2}).(0[0-9]|1[0-2]).(0[1-9]|[1-2][0-9]|3[0-1])$/;
-        console.log("birth 검사중");
-        let ckbirth = $("#birth-input").val();
-        if (ckbirth.trim().length == 0) {
-            $("#birth-input").text("출생연도를 입력해주세요").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
-        }else if(!birthCheck.test(ckbirth)) {
-            $("#birth-input").text("형식에 맞게 입력해주세요 ex)1999.01.01").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
+    $("#businessnum-input").keyup(function () {
+        let businessnumCheck =new RegExp('(\\d)');
+        console.log("businessnum 검사중");
+        let ckbuisinessnum = $("#businessnum-input").val();
+        if (ckbuisinessnum.trim().length === 0) {
+            $("#businessnum-result").text("사업자등록번호를 입력해주세요").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
+            return;
+        }else if(!businessnumCheck.test(ckbuisinessnum) || ckbuisinessnum.trim().length != 10) {
+            $("#businessnum-result").text("사업자등록번호를 다시 확인해주세요").removeClass('text-primary').removeClass('text-dark').addClass('text-danger');
+            return;
         }else {
-            $("#birth-input").empty();
+            $("#businessnum-result").empty();
+            return;
         }
     });
 
@@ -136,8 +146,9 @@ $(document).ready(function () {
 function submitForm() {
     console.log("submit() 실행");
     const specialRule = /[`~!@#$%^&*()_|+\-=?;:'"<>\{\}\[\]\\\/ ]/gim;
-    const emailCheck =/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    const birthCheck =/^(19[0-9][0-9]|20\d{2}).(0[0-9]|1[0-2]).(0[1-9]|[1-2][0-9]|3[0-1])$/;
+    const emailCheck =new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    const phoneCheck =new RegExp('(01[016789])(\\d{3,4})(\\d{4})');
+    const businessnumCheck =new RegExp('(\\d)');
     let ckid = $("#id-input").val();
     let ckpw = $("#pw-input").val();
     let ckname = $("#name-input").val();
@@ -146,10 +157,23 @@ function submitForm() {
     let ckdetail = $("#detail-input").val();
     let ckemail = $('#email-input').val();
     let ckphone = $("#phone-input").val();
-    let ckbirth = $("#birth-input").val();
+    let ckbusinessnum = $("#businessnum-input").val();
+
+    console.log(ckid);
+    console.log(ckpw);
+    console.log(ckname);
+    console.log(ckaddrno);
+    console.log(ckaddress);
+    console.log(ckdetail);
+    console.log(ckemail);
+    console.log(ckphone);
+    console.log(ckbusinessnum);
 
     if (ckid.length <= 0 || ckid.length > 8 || specialRule.test(ckid)) {
         alert("아이디를 다시 확인해주세요");
+        return;
+    }else if(!duplicateIdCheck){
+        alert("아이디 중복체크를 해주세요");
         return;
     }else if(ckpw.length < 8 || ckpw.length > 24 || ckpw.search(/\W|\s/g) > -1) {
         alert("비밀번호를 다시 확인해주세요");
@@ -157,44 +181,47 @@ function submitForm() {
     }else if(ckname.length < 2 || ckname.length > 16) {
         alert("이름을 다시 확인해주세요");
         return;
-    }else if(ckaddrno.length < 0) {
+    }else if(ckaddrno.length <= 0) {
         alert("우편번호 찾기를 진행해주세요");
         return;
-    }else if(ckdetail.length < 0) {
+    }else if(ckdetail.trim().length <= 0) {
         alert("상세주소를 다시 확인해주세요");
         return;
-    }else if(ckemail.trim().length < 0 || !emailCheck.test(ckemail)) {
+    }else if(ckemail.trim().length <= 0 || !emailCheck.test(ckemail)) {
+        console.log(ckaddrno);
+        console.log(ckaddrno.length);
+        console.log(ckdetail);
+        console.log(ckdetail.length);
         alert("이메일을 다시 확인해주세요");
-        return;
-    }else if(ckphone.length < 0 || ckphone.search(/\W|\s/g) > -1) {
-        alert("핸드폰 번호를 다시 확인해주세요");
-        return;
-    }else if(ckbirth.trim().length == 0 || !birthCheck.test(ckbirth)) {
-        alert("출생연도를 다시 확인해주세요");
-        return;
-    } else if(!duplicateIdCheck){
-        alert("아이디 중복체크를 해주세요");
         return;
     } else if(!duplicateEmailCheck) {
         alert("이메일 중복체크를 해주세요");
         return;
+    } else if(ckphone.length <= 0 || ckphone.search(/\W|\s/g) > -1 || !phoneCheck.test(ckphone)) {
+        alert("핸드폰 번호를 다시 확인해주세요");
+        return;
+    }else if(ckbusinessnum.trim().length < 10 || !businessnumCheck.test(ckbusinessnum)) {
+        alert("사업자등록번호를 다시 확인해주세요");
+        return;
     } else {
+        const obj = {
+            sellerName: ckid,
+            password: ckpw,
+            companyName: ckname,
+            address: ckaddress + ' ' + ckdetail,
+            email: ckemail,
+            phoneNum: ckphone,
+            businessRegNum: ckbusinessnum
+        }
         $.ajax({
-            url: "/api/v1/users/join",
+            url: "/api/v1/seller/join",
             method: "POST",
+            dataType: "json",
             contentType: 'application/json',
-            data: {
-                userName: ckid,
-                password: ckpw,
-                name: ckname,
-                address: ckaddress + ' ' + ckdetail,
-                email: ckemail,
-                phoneNum: ckphone,
-                birth: ckbirth.replaceAll('-', '.')
-            },
+            data: JSON.stringify(obj),
             success: function (data) {
-                alert(data.message);
-                location.href = "/users/login";
+                alert(data.result.message);
+                location.href = "/seller/login";
             }
         });
     }
@@ -209,7 +236,7 @@ function sample6_execDaumPostcode() {
             // 각 주소의 노출 규칙에 따라 주소를 조합한다.
             // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
             var addr = ''; // 주소 변수
-            // var extraAddr = ''; // 참고항목 변수
+            var extraAddr = ''; // 참고항목 변수
 
             //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
             if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
@@ -230,9 +257,9 @@ function sample6_execDaumPostcode() {
                     extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
                 }
                 // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-                // if(extraAddr !== ''){
-                //     extraAddr = ' (' + extraAddr + ')';
-                // }
+                if(extraAddr !== ''){
+                    extraAddr = ' (' + extraAddr + ')';
+                }
                 // 조합된 참고항목을 해당 필드에 넣는다.
                 // document.getElementById("sample6_extraAddress").value = extraAddr;
 
@@ -243,7 +270,7 @@ function sample6_execDaumPostcode() {
 
             // 우편번호와 주소 정보를 해당 필드에 넣는다.
             document.getElementById('address-no-input').value = data.zonecode;
-            document.getElementById("address-input").value = addr;
+            document.getElementById("address-input").value = addr + extraAddr;
             // 커서를 상세주소 필드로 이동한다.
             document.getElementById("detail-input").focus();
         }
